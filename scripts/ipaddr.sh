8668760f70a4cf9e587923eee9af881fe5a6b41a
petergs <pstratis11@gmail.com>
2023-04-12 18:51:52 -0400

    shellcheck

--- scripts/ipaddr.sh
+++ scripts/ipaddr.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-out=$(ip -4 addr show $1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | head -n1)
-if [[ $out -eq '' ]]; then 
+out=$(ip -4 addr show "$1" | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | head -n1)
+if [[ $out = '' ]]; then 
     echo 'disconnected'
 else 
-    echo $out
+    echo "$out"
 fi
