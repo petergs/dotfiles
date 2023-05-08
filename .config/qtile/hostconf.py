01d97dfac193ac308a307280987c717145066c9a
Peter Stratis <pstratis11@gmail.com>
2023-05-08 19:57:13 -0400

    Fix extended unicode references for icons
    Fixed extended unicode references to use the \U00000000 format
    in hostconf.py so that font icons are properly rendered.

--- .config/qtile/hostconf.py
+++ .config/qtile/hostconf.py
@@ -9,19 +9,19 @@ if hostname == "tomato":
     group_box_padding_x = 10
     spacer_len = 30
 
-    bright_char = "<U+F00E0>"
-    charge_char = "<U+F0084>"
-    discharge_char = "<U+F0079>"
-    empty_char = "<U+F007A>"
-    full_char = "<U+F0079>"
+    bright_char = "\U000F00E0"
+    charge_char = "\U000F0084"
+    discharge_char = "\U000F0079"
+    empty_char = "\U000F007A"
+    full_char = "\U000F0079"
 else:
     fontsize = 13
     barsize = 30
     group_box_padding_x = 7
     spacer_len = 25
 
-    bright_char = "<U+F5DF>"
-    charge_char = "<U+F583>"
-    discharge_char = "<U+F578>"
-    empty_char = "<U+F58D>"
-    full_char = "<U+F58E>"
+    bright_char = "\U0000F5DF"
+    charge_char = "\U0000F583"
+    discharge_char = "\U0000F578"
+    empty_char = "\U0000F58D"
+    full_char = "\U0000F58E"
